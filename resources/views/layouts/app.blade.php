26ab4805fa0721961814d32fe156d4898a8c85c6
Mees Hoogendoorn <mees018299@gmail.com>
2020-04-10 21:28:53 +0200

    fixed login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,11 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
+                    @guest
+                        Spotify API by mees
+                    @elseguest
                     {{ Auth::user()->name }}
+                    @endguest
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
