b97b4bc12cd49f2de3562cc8e07f24bb3fa8b11f
Mees Hoogendoorn <mees018299@gmail.com>
2020-04-10 20:43:29 +0200

    fixed site title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Spotify API by mees</title>
 
 
     <!-- Fonts -->
