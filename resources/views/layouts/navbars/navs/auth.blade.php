f0e8055d0b0c06cdaa8ba8b8a52f2d9d70172b40
Mees Hoogendoorn <mees018299@gmail.com>
2020-04-12 14:45:55 +0200

    Fixed logout button

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -4,10 +4,5 @@
         <!-- Brand -->
         <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="{{ route('home') }}">{{ __('Dashboard') }}</a>
         <!-- Form -->
-        <a href="{{ route('logout') }}" class="btn btn-outline-danger" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-            <i class="ni ni-user-run"></i>
-            <span>{{ __('Logout') }}</span>
-        </a>
     </div>
 </nav>
