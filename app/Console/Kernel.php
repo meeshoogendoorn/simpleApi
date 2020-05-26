31910b5a4b20e29ea028a3c1d17f0ec921b3bef0
Mees Hoogendoorn <mees018299@gmail.com>
2020-05-26 09:33:33 +0200

    Added cron in laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//         $schedule->command('players:deactivate')->cron("1 */2 * * *");
+         $schedule->command('players:deactivate')->cron("1 */2 * * *");
     }
 
     /**
